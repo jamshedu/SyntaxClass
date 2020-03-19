00f37ca079384b7e733ffd47f3ac46673687ae31
Jamshedu <jamshed1988@yahoo.com>
2020-03-18 23:58:34 -0700

    Second commit

--- src/com/syntaxclass13/Q2.java
+++ src/com/syntaxclass13/Q2.java
@@ -3,18 +3,29 @@ package com.syntaxclass13;
 public class Q2 {
 
 	public static void main(String[] args) {
+		/*
+		 * Write a java program to check whether a given number is prime or not?
+		 * 
+		 * prime number is a number that is greater than 1
+		 * prime number should meet 2 conditions(divisible by 1 and itself only)
+		 * 2(1,2),3(2,3),5(2,3,4,5),7(2,3,4,5,6,7),11,13,17
+		 */
+
+		int given = 10;
+		boolean isPrime=true;
 		
-		int[] fib = new int[10];
-		fib[0] = 0;
-		fib[1] = 1;
-		for (int i = 2; i < 10; i++) {// to find rest fibonacci until 10
-			fib[i] = fib[i - 1] + fib[i - 2];
+		if(given>1) {
+			for(int i=2; i<given; i++) {
+				if(given%i==0) {
+					isPrime=false;
+					break;
+				}
+			}
+			
+		}else {
+			isPrime=false;
 		}
-		for (int j = 0; j < 10; j++) {// number of index
-			System.out.print(fib[j] + " ");
-		
-		}
+		System.out.println("Given number "+given+" is prime? "+isPrime);
 		
 	}
-
 }
--- src/com/syntaxclass13/Q5.java
+++ src/com/syntaxclass13/Q5.java
@@ -4,17 +4,27 @@ public class Q5 {
 
 	public static void main(String[] args) {
 		
+	/*
+	 * Reverse String character by character
+	 * word by word
+	 */
+
+		String given="I love java";
+		String reversed="";
 		
-		String given="I love Java";
-		String reversed ="";
-		
-		for (int i =given.length()-1; i>=0; i--) {
-			reversed =reversed+given.charAt(i);
+		for(int i=given.length()-1; i>=0; i--) {
+			reversed=reversed+given.charAt(i);
 		}
 		
 		System.out.println("Reversed String = "+reversed);
 		
-
+		String[] array=given.split(" ");
+		String reverseWords="";
+		
+		for(int i=array.length-1; i>=0; i--) {
+			reverseWords=reverseWords+array[i]+" ";
+		}
+		
+		System.out.println("Reversed String word by word= "+reverseWords);
 	}
-
 }
